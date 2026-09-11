1725025317a794d00b059dc6b7b87268153d3105
Anup Kushwaha <anupkushwaha916@gmail.com>
2026-09-11 23:38:26 +0530

    Expressjs

---
+++ Day1-Nodejs/server.js
@@ -0,0 +1,18 @@
+// console.log("Anup singh");
+// console.log("kya haal h tumhare");
+
+// console.log("khana kha liya kya apne agar nhi khaya to khakar aaiye");
+
+
+let http = require("http");
+
+let server = http.createServer((req, res)=>{
+    console.log("Hello");
+    
+});
+
+server.listen(()=>{
+    console.log("This is on port 3000");
+    
+})
+
